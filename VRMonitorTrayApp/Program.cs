1a1038e0fa66f7dccf3795ddcae72a83c4a4d5c7
Eliminater74 <eliminater74@gmail.com>
2023-09-17 18:13:03 -0400

    Some more Changes updated

--- VRMonitorTrayApp/Program.cs
+++ VRMonitorTrayApp/Program.cs
@@ -1,16 +1,156 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.ServiceProcess;
-using System.Threading;
 using System.Windows.Forms;
 
 public class VRMonitorTrayApp
 {
-    private static NotifyIcon trayIcon;
-    private static System.Threading.Timer monitoringTimer; // Specify the Timer from System.Threading
-    private static bool isMonitoring = true;
+    private NotifyIcon trayIcon;
+    private System.Windows.Forms.Timer monitoringTimer;
+    private HashSet<string> monitoredProcessesAndServices = new HashSet<string>();
+    private string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+
+    public VRMonitorTrayApp()
+    {
+        trayIcon = new NotifyIcon()
+        {
+            Icon = SystemIcons.Application,
+            ContextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    new ToolStripMenuItem("Start Monitoring", null, StartMonitoring),
+                    new ToolStripMenuItem("Stop Monitoring", null, StopMonitoring),
+                    new ToolStripMenuItem("Show Log", null, ShowLog),
+                    new ToolStripMenuItem("Exit", null, Exit)
+                }
+            },
+            Visible = true
+        };
+
+        monitoringTimer = new System.Windows.Forms.Timer
+        {
+            Interval = 5000 // 5 seconds
+        };
+        monitoringTimer.Tick += MonitorProcessesAndServices;
+
+        // Log already running Oculus and SteamVR processes and services
+        LogInitialProcessesAndServices();
+    }
+
+    private void StartMonitoring(object sender, EventArgs e)
+    {
+        monitoringTimer.Start();
+    }
+
+    private void StopMonitoring(object sender, EventArgs e)
+    {
+        monitoringTimer.Stop();
+    }
+
+    private void ShowLog(object sender, EventArgs e)
+    {
+        Process.Start("notepad.exe", logFilePath);
+    }
+
+    private void Exit(object sender, EventArgs e)
+    {
+        trayIcon.Visible = false;
+        Application.Exit();
+    }
+
+    private void MonitorProcessesAndServices(object sender, EventArgs e)
+    {
+        MonitorProcesses();
+        MonitorServices();
+    }
+
+    private void MonitorProcesses()
+    {
+        var currentProcesses = Process.GetProcesses()
+            .Where(p => p.ProcessName.Contains("Oculus") || p.ProcessName.Contains("SteamVR"))
+            .ToList();
+
+        foreach (var process in currentProcesses)
+        {
+            string processDetails = $"{process.ProcessName} ({process.MainModule.FileName})";
+            if (!monitoredProcessesAndServices.Contains(processDetails))
+            {
+                monitoredProcessesAndServices.Add(processDetails);
+                Log($"{DateTime.Now}: {processDetails} started.");
+            }
+        }
+
+        var stoppedProcesses = monitoredProcessesAndServices.Except(currentProcesses.Select(p => $"{p.ProcessName} ({p.MainModule.FileName})")).ToList();
+        foreach (var process in stoppedProcesses)
+        {
+            monitoredProcessesAndServices.Remove(process);
+            Log($"{DateTime.Now}: {process} stopped.");
+        }
+    }
+
+    private void MonitorServices()
+    {
+        var currentServices = ServiceController.GetServices()
+            .Where(s => s.DisplayName.Contains("Oculus") || s.DisplayName.Contains("SteamVR"))
+            .ToList();
+
+        foreach (var service in currentServices)
+        {
+            string serviceDetails = $"{service.ServiceName} ({service.DisplayName})";
+            if (!monitoredProcessesAndServices.Contains(serviceDetails))
+            {
+                monitoredProcessesAndServices.Add(serviceDetails);
+                Log($"{DateTime.Now}: Service {serviceDetails} started.");
+            }
+        }
+
+        var stoppedServices = monitoredProcessesAndServices.Except(currentServices.Select(s => $"{s.ServiceName} ({s.DisplayName})")).ToList();
+        foreach (var service in stoppedServices)
+        {
+            monitoredProcessesAndServices.Remove(service);
+            Log($"{DateTime.Now}: Service {service} stopped.");
+        }
+    }
+
+    private void LogInitialProcessesAndServices()
+    {
+        var initialProcesses = Process.GetProcesses()
+            .Where(p => p.ProcessName.Contains("Oculus") || p.ProcessName.Contains("SteamVR"))
+            .ToList();
+
+        foreach (var process in initialProcesses)
+        {
+            string processDetails = $"{process.ProcessName} ({process.MainModule.FileName})";
+            if (!monitoredProcessesAndServices.Contains(processDetails))
+            {
+                monitoredProcessesAndServices.Add(processDetails);
+                Log($"{DateTime.Now}: {processDetails} was already running when the application started.");
+            }
+        }
+
+        var initialServices = ServiceController.GetServices()
+            .Where(s => s.DisplayName.Contains("Oculus") || s.DisplayName.Contains("SteamVR"))
+            .ToList();
+
+        foreach (var service in initialServices)
+        {
+            string serviceDetails = $"{service.ServiceName} ({service.DisplayName})";
+            if (!monitoredProcessesAndServices.Contains(serviceDetails))
+            {
+                monitoredProcessesAndServices.Add(serviceDetails);
+                Log($"{DateTime.Now}: Service {serviceDetails} was already running when the application started.");
+            }
+        }
+    }
+
+    private void Log(string message)
+    {
+        File.AppendAllText(logFilePath, message + Environment.NewLine);
+    }
 
     [STAThread]
     public static void Main()
@@ -18,107 +158,7 @@ public class VRMonitorTrayApp
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
 
-        InitializeTrayIcon();
-
-        monitoringTimer = new System.Threading.Timer(MonitorProcesses, null, TimeSpan.Zero, TimeSpan.FromSeconds(10)); // Specify the Timer from System.Threading
-
+        var app = new VRMonitorTrayApp();
         Application.Run();
-    }
-
-    private static void InitializeTrayIcon()
-    {
-        trayIcon = new NotifyIcon()
-        {
-            Icon = SystemIcons.Application,
-            ContextMenuStrip = new ContextMenuStrip(), // Use ContextMenuStrip
-            Visible = true
-        };
-
-        // Add ToolStripMenuItems to the ContextMenuStrip
-        trayIcon.ContextMenuStrip.Items.Add("Toggle Monitoring", null, ToggleMonitoring);
-        trayIcon.ContextMenuStrip.Items.Add("Restart Oculus Runtime", null, RestartOculusRuntime);
-        trayIcon.ContextMenuStrip.Items.Add("About", null, OnAbout);
-        trayIcon.ContextMenuStrip.Items.Add("Exit", null, OnExit);
-    }
-
-    private static void ToggleMonitoring(object sender, EventArgs e)
-    {
-        isMonitoring = !isMonitoring;
-        string status = isMonitoring ? "Monitoring started" : "Monitoring paused";
-        trayIcon.ShowBalloonTip(1000, "Status", status, ToolTipIcon.Info);
-    }
-
-    private static void RestartOculusRuntime(object sender, EventArgs e)
-    {
-        try
-        {
-            ServiceController sc = new ServiceController("OVRService");
-            if (sc.Status == ServiceControllerStatus.Running)
-            {
-                sc.Stop();
-                sc.WaitForStatus(ServiceControllerStatus.Stopped);
-                sc.Start();
-                trayIcon.ShowBalloonTip(1000, "Status", "Oculus Runtime restarted", ToolTipIcon.Info);
-            }
-        }
-        catch (Exception ex)
-        {
-            trayIcon.ShowBalloonTip(1000, "Error", "Failed to restart Oculus Runtime: " + ex.Message, ToolTipIcon.Error);
-        }
-    }
-
-    private static void OnAbout(object sender, EventArgs e)
-    {
-        trayIcon.ShowBalloonTip(1000, "About", "VR Monitor Tray App", ToolTipIcon.Info);
-    }
-
-    private static void OnExit(object sender, EventArgs e)
-    {
-        trayIcon.Visible = false;
-        Application.Exit();
-    }
-
-    private static void MonitorProcesses(object state)
-    {
-        if (!isMonitoring) return;
-
-        try
-        {
-            var processes = Process.GetProcesses();
-            var currentOVRProcesses = processes.Where(p => p.ProcessName.Contains("OVR")).ToList();
-            var previousOVRProcesses = new List<string>(); // Store the names of previously detected OVR processes
-
-            // Check for new processes
-            foreach (var process in currentOVRProcesses)
-            {
-                if (!previousOVRProcesses.Contains(process.ProcessName))
-                {
-                    Log($"Process started: {process.ProcessName}");
-                }
-            }
-
-            // Check for stopped processes
-            foreach (var processName in previousOVRProcesses)
-            {
-                if (!currentOVRProcesses.Any(p => p.ProcessName == processName))
-                {
-                    Log($"Process stopped: {processName}");
-                }
-            }
-
-            // Update the list of previous processes
-            previousOVRProcesses = currentOVRProcesses.Select(p => p.ProcessName).ToList();
-        }
-        catch (Exception ex)
-        {
-            Log($"Error monitoring processes: {ex.Message}");
-        }
-    }
-
-
-    private static void Log(string message)
-    {
-        string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
-        File.AppendAllText(logPath, $"{DateTime.Now}: {message}\n");
     }
 }
